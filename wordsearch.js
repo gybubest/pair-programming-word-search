d8cf487b6246e0b889b4960c5867aff3b94f5bd4
Yu Gao <gaoyububest@gmail.com>
2020-11-04 22:38:15 +0000

    wordSearch

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  for (let i = 0; i < letters.length; i++) {
+    let string = '';
+    for (let j = 0; j < letters[i].length; j++) {
+      string += letters[i][j];
+      if (string.includes(word)) {
+        return true;
+      }
     }
-}
+  }
+  for (let i = 0; i < letters[0].length; i++) {
+    let string = '';
+    for (let j = 0; j < letters.length; j++) {
+      string += letters[i][j];
+      if (string.includes(word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
+// **** The following program returns true when the word is broken into different rows/collumns. Pair-programming with @m-vojjala. ****
+// const wordSearch = (letters, word) => {
+//     let row = "";
+//     let column = "";
+//     for (let i = 0; i < letters[0].length; i++) {
+//       for (let j = 0; j < letters[i].length; j++) {
+//         row += letters[i][j];
+//         column += letters[j][i];
+//         if (row.includes(word)) {
+//           return true;
+//         } else if (column.includes(word)) {
+//           return true;
+//         }
+//       }
+//     }
+//     return false;
+//   };
